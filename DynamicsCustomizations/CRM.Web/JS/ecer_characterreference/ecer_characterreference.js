c577ed737f9def781f1c8358fdca7d8f97e2544f
Andrew Chan <andrew.chan@quartech.com>
2024-03-05 18:28:22 -0700

    ECER-1161 Add Form Script to Character Reference and Work Experience Ref

---
+++ DynamicsCustomizations/CRM.Web/JS/ecer_characterreference/ecer_characterreference.js
@@ -0,0 +1,42 @@
+﻿// Javascript source code
+
+if (typeof ECER === "undefined") {
+    var ECER = {}
+}
+
+if (typeof ECER.Jscripts === "undefined") {
+    ECER.Jscripts = {}
+}
+
+ECER.Jscripts.CharacterReferemce = {
+    onLoad: function (executionContext) {
+        ECER.Jscripts.CharacterReferemce.showHideOnProvinceSelected(executionContext);
+    },
+
+
+    showHideOnProvinceSelected: function (executionContext) {
+        // ECER-1161
+        // Lookup for Province.  If selected Other, then show the text field and make that required.
+        // Other Province GUID
+        var otherProvinceGUID = "5e291527-7ada-ee11-904c-002248d56374";
+        var formContext = executionContext.getFormContext();
+        var lookupAttributeName = "ecer_referencecertifiedprovinceid";
+        var textAttributeName = "ecer_referencecertifiedprovince";
+        var lookupAttribute = formContext.getAttribute(lookupAttributeName);
+        var show = false;
+        var required = "none";
+        if (lookupAttribute != null && lookupAttribute.getValue() != null) {
+            var lookupValue = lookupAttribute.getValue()[0].id.toLowerCase().replace("{", "").replace("}", "");
+            if (lookupValue == otherProvinceGUID) {
+                required = "required";
+                show = true;
+            }
+        }
+
+        crm_Utility.showHide(executionContext, show, textAttributeName);
+        crm_Utility.setRequiredLevel(executionContext, required, textAttributeName);
+
+    }
+}
+
+
