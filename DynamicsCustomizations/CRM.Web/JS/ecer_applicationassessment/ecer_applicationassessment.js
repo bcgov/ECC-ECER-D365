c48c212ab623892424b2e8201e2580c186df7da1
Andrew Chan <andrew.chan@quartech.com>
2024-07-30 19:04:26 -0600

    Simplify solution for QV of Character Reference

--- DynamicsCustomizations/CRM.Web/JS/ecer_applicationassessment/ecer_applicationassessment.js
+++ DynamicsCustomizations/CRM.Web/JS/ecer_applicationassessment/ecer_applicationassessment.js
@@ -10,53 +10,87 @@ if (typeof ECER.Jscripts === "undefined") {
 
 ECER.Jscripts.ApplicationAssessments = {
     onLoad: function (executionContext) {
-        // Disable Editable Grid Columns
+        ECER.Jscripts.ApplicationAssessments.populateCharacterReferenceLookupIfEmpty(executionContext);
     },
 
-    disableEducationAssessmentGridColumns: function (executionContext) {
-        //Some columns should not be editable
-        if (!executionContext) {
-            console.error("Execution context is not defined.");
-            return;
-        }
-
+    populateCharacterReferenceLookupIfEmpty: function (executionContext) {
+        // ECER-2246
         var formContext = executionContext.getFormContext();
 
-        var stateCodeAttribute = formContext.getAttribute("statecode");
-        if (stateCodeAttribute && stateCodeAttribute.getValue() === 1) {
+        var characterReferenceAttribute = formContext.getAttribute("ecer_characterreferencereviewedid");
+        if (characterReferenceAttribute !== null && characterReferenceAttribute.getValue() !== null) {
+            return; // Only populating if there is NULL value
+        }
+
+        var applicationAttribute = formContext.getAttribute("ecer_applicationid");
+        if (applicationAttribute === null || applicationAttribute.getValue() === null) {
             return;
         }
 
-        formContext.getData().getEntity().attributes.forEach(function (attr) {
-            attr.controls.forEach(function (control) {
-                control.setDisabled(true);
-            });
-        });
-
-        var userSettings = Xrm.Utility.getGlobalContext().userSettings;
-        var currentUserId = userSettings.userId;
-
-        var createdBy = formContext.getAttribute("createdby").getValue();
-        var createdById = createdBy ? createdBy[0].id : "";
-
-        var ownerUser = formContext.getAttribute("ownerid").getValue();
-        var ownerUserId = ownerUser ? ownerUser[0].id : "";
-
-        var createdOn = formContext.getAttribute("createdon").getValue();
-        var createdOnDate = createdOn ? createdOn.toDateString() : "";
-
-        var today = new Date().toDateString();
-
-        if ((currentUserId === createdById || currentUserId === ownerUserId) && createdOnDate === today) {
-            formContext.getData().getEntity().attributes.forEach(function (attr) {
-                var attrName = attr.getName();
-                if (attrName === "ecer_notes" || attrName === "ecer_decision") {
-                    attr.controls.forEach(function (control) {
-                        control.setDisabled(false);
-                    });
+        var applicationId = applicationAttribute.getValue()[0].id.toLowerCase().replace("{", "").replace("}", "");
+        // Character Reference of the Application that is NOT DRAFT, NOT Application Submitted, and NOT Rejected
+        var option = "?$filter=_ecer_applicationid_value eq '" + applicationId + "' and statuscode ne 1 and statuscode ne 621870006 and statuscode ne 621870005";
+        Xrm.WebApi.retrieveMultipleRecords("ecer_characterreference", option).then(
+            function success(results) {
+                if (results.entities.length > 0) {
+                    var entityRecordId = results.entities[0].ecer_characterreferenceid;
+                    var entityRecordName = results.entities[0].ecer_name;
+                    var lookupArray = new Array();
+                    lookupArray[0] = new Object();
+                    // Treat the entity record to include curly braces if needed
+                    if (entityRecordId.indexOf("{") === -1) {
+                        entityRecordId = "{" + entityRecordId + "}";
+                    }
+                    lookupArray[0].id = entityRecordId;
+                    lookupArray[0].name = entityRecordName;
+                    lookupArray[0].entityType = "ecer_characterreference";
+                    characterReferenceAttribute.setValue(lookupArray);
                 }
-            });
+            }
+        );
+    },
+
+    characterReferenceGridPreFilter: function (executionContext) {
+        // Reference: https://learn.microsoft.com/en-us/power-apps/developer/model-driven-apps/clientapi/reference/controls/addcustomfilter
+        // ECER-2246
+        // Leave the method here.  Doesn't really need a subgrid as a prefilter medium
+        // Just used a Pre-Filter Lookup
+        var formContext = executionContext.getFormContext();
+        var applicationAttribute = formContext.getAttribute("ecer_applicationid");
+        if (applicationAttribute === null || applicationAttribute.getValue() === null) {
+            return;
         }
+
+        var applicationId = applicationAttribute.getValue()[0].id.toLowerCase().replace("{", "").replace("}", "");
+
+        var fetchXML = "<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+            "<entity name = 'ecer_characterreference'>" +
+            "<attribute name='ecer_characterreferenceid' />" +
+            "<attribute name='ecer_firstname' />" +
+            "<attribute name='ecer_lastname' />" +
+            "<attribute name='ecer_emailaddress' />" +
+            "<attribute name='ecer_phonenumber' />" +
+            "<attribute name='ecer_knownapplicanttimechoice' />" +
+            "<attribute name='ecer_relationshiptoapplicant' />" +
+            "<attribute name='ecer_applicationid' />" +
+            "<order attribute='ecer_lastname' descending='false' />" +
+            "<filter type='and'>" +
+            "<condition attribute='ecer_applicationid' operator='eq' value='" + applicationId + "' />" +
+            "</filter>" +
+            "</entity >" +
+            "</fetch >";
+
+        var characterReferenceSubgridControl = formContext.getControl("subgrid_characterreference");
+        if (characterReferenceSubgridControl !== null) {
+
+            characterReferenceSubgridControl.setFilterXml(fetchXML);
+            characterReferenceSubgridControl.refresh();
+        }
+        else {
+            // Run 2 seconds later if subgrid is not loaded.
+            setTimeout(ECER.Jscripts.ApplicationAssessments.characterReferenceGridPreFilter(executionContext), 2000);
+        }
+
     }
 }
 
