25c69a3dd81b0aa60ffd3f342a72421f69babeaa
ChoVamsi <Vamsi.Choudary@gov.bc.ca>
2025-04-14 09:21:37 -0700

    ECER-4464

---
+++ DynamicsCustomizations/CRM.Web/JS/ecer_investigationplanningallegation/ecer_investigationplanningallegation.js
@@ -0,0 +1,37 @@
+﻿// JavaScript source code
+
+if (typeof ECER === "undefined") {
+    var ECER = {};
+}
+
+if (typeof ECER.Jscripts === "undefined") {
+    ECER.Jscripts = {};
+}
+
+ECER.Jscripts.Allegation = {
+    onSave: function (executionContext) {
+        this.onChangeDetails(executionContext);
+    },
+    onChangeDetails: function (executionContext) {
+        // check if details is dirty
+        let formContext = executionContext.getFormContext();
+
+        let details = formContext.getAttribute("ecer_details");
+
+        if (details === null
+            || !details.getIsDirty()
+            || !DOMParser) {
+            return;
+        }
+
+        if (details.getValue() != null) {
+            //ecer_detailstext is has string data from HTML in ecer_details
+            var parsedString = (new DOMParser()).parseFromString(details.getValue(), 'text/html');
+
+            formContext.getAttribute("ecer_detailstext")?.setValue(parsedString?.body?.textContent);
+        }
+        else {
+            formContext.getAttribute("ecer_detailstext")?.setValue(null);
+        }
+    }
+}
