afdbcf3cca7941a3fe05ea60ea756164f24088e4
Tobit Sabu <Tobit.Sabu@gov.bc.ca>
2025-04-29 22:58:46 -0700

    ECER_4698: adding ecer_email.js. Setting the  'From' field based on the security role

---
+++ DynamicsCustomizations/CRM.Web/JS/ecer_email/ecer_email.js
@@ -0,0 +1,60 @@
+if (typeof ECER === "undefined") {
+    var ECER = {};
+}
+
+if (typeof ECER.Jscripts === "undefined") {
+    ECER.Jscripts = {};
+}
+
+
+ECER.Jscripts.Email =
+{
+    onLoad: function (executionContext) {
+        
+        ECER.Jscripts.Email.setFromLookup(executionContext);
+       
+        
+    },
+    //ECER-4698
+    setFromLookup(executionContext){
+
+        var formContext = executionContext.getFormContext();
+        var from = null;
+        
+        var formType = formContext.ui.getFormType();
+        if (formType !== 1) {// check if its create
+            return;
+        }
+
+        var programSupportRole = crm_Utility.checkCurrentUserRole("Certification - Program Support Role");
+        var programSupportLeadRole = crm_Utility.checkCurrentUserRole("Certification - Program Support Lead Role");
+
+        var pspProgramAnalystRole = crm_Utility.checkCurrentUserRole("PSP - Program Analyst");
+        var pspProgramCodinatorRole = crm_Utility.checkCurrentUserRole("PSP - Program Coordinator");
+        var pspProgramDirectorRole = crm_Utility.checkCurrentUserRole("PSP - Program Director");
+      
+
+        if(pspProgramAnalystRole||  pspProgramCodinatorRole || pspProgramDirectorRole){
+            from = this.getQueueLookup("ECERegistry.Programs@gov.bc.ca");
+        }
+        else if(programSupportRole || programSupportLeadRole){
+            from = this.getQueueLookup("ECERegistry.Intake@gov.bc.ca");
+        }else{
+            from = this.getQueueLookup("ECERegistry@gov.bc.ca");
+        }
+        if(from){
+            var fromlookup = crm_Utility.generateLookupObject("queue", from.queueid, from.name);
+            crm_Utility.setLookupValue(executionContext,"from", fromlookup);
+        }
+
+    },
+    getQueueLookup(queueName){
+       
+        var queue = null;
+    
+        var queueQueryOption = `?$filter=name eq '${queueName}' and statecode eq 0`;
+        queue = crm_Utility.retrieveMultipleCustom("queue", queueQueryOption);
+        return queue[0];
+    }
+  
+}
