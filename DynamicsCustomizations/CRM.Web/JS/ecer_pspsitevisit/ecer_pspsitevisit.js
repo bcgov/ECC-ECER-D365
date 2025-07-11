c45e5a3ff2dfb635df6e7ac3468cdcf976638afd
Andrew Chan <andrew.chan@quartech.com>
2025-07-11 14:54:51 -0600

    ECER-5053 PSI Program Representative lookup changes

--- DynamicsCustomizations/CRM.Web/JS/ecer_pspsitevisit/ecer_pspsitevisit.js
+++ DynamicsCustomizations/CRM.Web/JS/ecer_pspsitevisit/ecer_pspsitevisit.js
@@ -18,9 +18,8 @@ ECER.Jscripts.PSPSiteVisit =
 
     setPSPRepresentative: function (executionContext) {
         var formContext = executionContext.getFormContext();
-
-        var psp = formContext.getAttribute("ecer_postsecondaryinstitution").getValue();
-        var pspRep = formContext.getAttribute("ecer_programrepresentativeid");
+        var psp = formContext.getAttribute("ecer_programapplication").getValue();
+        var pspRep = formContext.getAttribute("ecer_psiprogramrepresentative");
 
         if (psp == null) {
             pspRep.setValue(null);
@@ -29,12 +28,13 @@ ECER.Jscripts.PSPSiteVisit =
 
         var pspId = psp[0].id;
         var pspEntityType = psp[0].entityType;
-        var columnSet = "_ecer_primaryprogramrepresentative_value";
+        var columnSet = "_ecer_psiprogramrepresentative_value";
 
         Xrm.WebApi.retrieveRecord(pspEntityType, pspId, "?$select=" + columnSet).then(
             function success(result) {
                 console.log(result);
-                var pspRepId = result["_ecer_primaryprogramrepresentative_value"]
+                var pspRepId = result["_ecer_psiprogramrepresentative_value"]
+
                 if (pspRepId == null) {
                     pspRep.setValue(null);
                     return;
@@ -42,16 +42,16 @@ ECER.Jscripts.PSPSiteVisit =
                 var pspRepValue = [];
                 pspRepValue[0] = {};
                 pspRepValue[0].id = pspRepId;
-                pspRepValue[0].name = result["_ecer_primaryprogramrepresentative_value@OData.Community.Display.V1.FormattedValue"];
-                pspRepValue[0].entityType = result["_ecer_primaryprogramrepresentative_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
-
-                ECER.Jscripts.PSPSiteVisit.setLookupField(formContext, "ecer_programrepresentativeid", pspRepValue);
+                pspRepValue[0].name = result["_ecer_psiprogramrepresentative_value@OData.Community.Display.V1.FormattedValue"];
+                pspRepValue[0].entityType = result["_ecer_psiprogramrepresentative_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
+                ECER.Jscripts.PSPSiteVisit.setLookupField(formContext, "ecer_ecer_psiprogramrepresentative", pspRepValue);
             },
             function (error) {
                 console.log(error.message);
                 pspRep.setValue(null);;
             }
         );
+
     },
 
     setLookupField: function (formContext, fieldName, fieldValue) {
@@ -63,7 +63,7 @@ ECER.Jscripts.PSPSiteVisit =
             && fieldValue[0].entityType != null) {
 
             var field = formContext.getAttribute(fieldName);
-            if(field != null){
+            if (field != null) {
                 field.setValue(fieldValue);
             }
         }
