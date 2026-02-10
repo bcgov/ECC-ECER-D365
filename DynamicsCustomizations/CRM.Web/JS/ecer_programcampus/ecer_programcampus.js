14d24a2d3519c86239d0e43a7ec581543d4a61e9
ChoVamsi <Vamsi.Choudary@gov.bc.ca>
2026-02-09 20:52:55 -0800

    ECER-5840

--- DynamicsCustomizations/CRM.Web/JS/ecer_programcampus/ecer_programcampus.js
+++ DynamicsCustomizations/CRM.Web/JS/ecer_programcampus/ecer_programcampus.js
@@ -13,6 +13,56 @@ ECER.Jscripts.ProgramCampus =
         var formContext = executionContext.getFormContext();
 
         this.mandatoryBasedOnCampus(executionContext);
+        this.setDefaultsOnCreate(executionContext);
+    },
+    setDefaultsOnCreate: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+
+        let programProfile = formContext.getAttribute("ecer_programprofileid").getValue();
+        let programApplication = formContext.getAttribute("ecer_programapplicationid").getValue();
+
+        if (programProfile !== null) {
+            Xrm.WebApi.retrieveRecord("ecer_program", programProfile[0].id, "?$select=_ecer_postsecondaryinstitution_value").then(
+                function success(result) {
+                    let educationalInstitution = result._ecer_postsecondaryinstitution_value;
+                    if (educationalInstitution !== null) {
+                        formContext.getAttribute("ecer_educationalinstitutionid").setValue([
+                            {
+                                id: educationalInstitution,
+                                name: result["_ecer_postsecondaryinstitution_value@OData.Community.Display.V1.FormattedValue"], 
+                                entityType: "ecer_postsecondaryinstitute"
+                            }
+                        ]);
+
+                        formContext.getAttribute("ecer_educationalinstitutionid").setSubmitMode("dirty");
+                    }
+                },
+                function (error) {
+                    console.log(error.message);
+                }
+            );
+        }
+        else if (programApplication !== null) {
+            Xrm.WebApi.retrieveRecord("ecer_postsecondaryinstituteprogramapplicaiton", programApplication[0].id, "?$select=_ecer_postsecondaryinstitute_value").then(
+                function success(result) {
+                    let educationalInstitution = result._ecer_postsecondaryinstitute_value;
+                    if (educationalInstitution !== null) {
+                        formContext.getAttribute("ecer_educationalinstitutionid").setValue([
+                            {
+                                id: educationalInstitution,
+                                name: result["_ecer_postsecondaryinstitute_value@OData.Community.Display.V1.FormattedValue"], 
+                                entityType: "ecer_postsecondaryinstitute"
+                            }
+                        ]);
+
+                        formContext.getAttribute("ecer_educationalinstitutionid").setSubmitMode("dirty");
+                    }
+                },
+                function (error) {
+                    console.log(error.message);
+                }
+            );
+        }
     },
     mandatoryBasedOnCampus: function (executionContext) {
         var formContext = executionContext.getFormContext();
